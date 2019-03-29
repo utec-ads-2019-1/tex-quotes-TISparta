52616beac5bc19ae3b3830471d6fef6c8c41fc68
TISparta <jose.garcia@utec.edu.pe>
2019-03-28 20:44:46 -0500

    Solucion

--- main.cpp
+++ main.cpp
@@ -1,7 +1,25 @@
-#include <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-int main(int argc, char *argv[]) {
-    return 0;
+int main () {
+  string line;
+  bool opened = false;
+  while (getline(cin, line)) {
+    for (char ch: line) {
+      if (ch != '\"') {
+        cout << ch;
+        continue;
+      }
+      if (not opened) {
+        cout << "``";
+        opened = true;
+      } else {
+        cout << "''";
+        opened = false;
+      }
+    }
+    cout << endl;
+  }
+  return (0);
 }
